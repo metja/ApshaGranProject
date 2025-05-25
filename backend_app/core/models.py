3b07ded52bbd8b994938e6cc872a5b3e4e8b9647
Bohdan11Dii <bodyabrovdiy@gmail.com>
2025-05-25 17:11:34 +0200

    Create models and makemigrations, add urls and add first ViewSet class

--- backend_app/core/models.py
+++ backend_app/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.name
+    
--- backend_app/core/urls.py
+++ backend_app/core/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
-from core.views import web_site
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from core.views import web_site, CategoryViewSet
+
+
+router = DefaultRouter()
+router.register('categories', CategoryViewSet)
 
 urlpatterns = [
     path("", web_site, name="web-site"),
+    path('', include(router.urls)),
 ]
 
 
